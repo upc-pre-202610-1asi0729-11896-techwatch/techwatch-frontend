dcb2038e237cc9bf2d2ad487de522ed21ab3948c
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-05-10 19:32:49 -0500

    feat: add dependencies for material and project structure

---
+++ src/app/shared/presentation/components/toolbar/toolbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.css',
+})
+export class Toolbar {}
