eec329b51826276999adf8e42566b09dc3e4474a
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-05-11 18:05:15 -0500

    feat: add api for devices

--- src/app/management/infrastructure/management-api.ts
+++ src/app/management/infrastructure/management-api.ts
@@ -2,21 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import {HomeApiEndpoints} from './home/home-api-endpoints';
 import {BaseApi} from '../../shared/interface/base-api';
+
+import {HomeApiEndpoints} from './home/home-api-endpoints';
 import {HomeEntity} from '../domain/models/home/home-entity';
+
+import {DeviceEntity} from '../domain/models/device/device-entity';
+import {DevicesApiEndpoints} from './device/devices-api-endpoints';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ManagementApi extends BaseApi {
   private readonly homeEndpoint: HomeApiEndpoints;
+  private readonly deviceEndpoint: DevicesApiEndpoints;
 
   constructor(http: HttpClient) {
     super();
     this.homeEndpoint = new HomeApiEndpoints(http);
+    this.deviceEndpoint = new DevicesApiEndpoints(http);
   }
-
+  //
+  // Homes
   getHomes(): Observable<HomeEntity[]> {
     return this.homeEndpoint.getAll();
   }
@@ -35,5 +42,25 @@ export class ManagementApi extends BaseApi {
 
   deleteHome(id: number): Observable<void> {
     return this.homeEndpoint.delete(id);
+  }
+  // Devices
+  getDevices(): Observable<DeviceEntity[]> {
+    return this.deviceEndpoint.getAll();
+  }
+
+  getDevice(id: number): Observable<DeviceEntity> {
+    return this.deviceEndpoint.getById(id);
+  }
+
+  createDevice(device: DeviceEntity): Observable<DeviceEntity> {
+    return this.deviceEndpoint.create(device);
+  }
+
+  updateDevice(device: DeviceEntity): Observable<DeviceEntity> {
+    return this.deviceEndpoint.update(device, device.id);
+  }
+
+  deleteDevice(id: number): Observable<void> {
+    return this.deviceEndpoint.delete(id);
   }
 }
