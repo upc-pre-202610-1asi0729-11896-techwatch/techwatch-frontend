9257963904904de422636a3f5f92e65ab8b88576
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-05-10 20:55:25 -0500

    feat: add base toolbar component

--- src/app/shared/presentation/components/toolbar/toolbar.ts
+++ src/app/shared/presentation/components/toolbar/toolbar.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLinkActive,
+  ],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.css',
 })
-export class Toolbar {}
+export class Toolbar {
+  options = signal([
+    {link: '/home',                 label: 'Home'},
+    {link: '/mangament/homes',      label: 'Homes'},
+    {link: '/mangament/devices',    label: 'Devices'}
+  ]);
+}
