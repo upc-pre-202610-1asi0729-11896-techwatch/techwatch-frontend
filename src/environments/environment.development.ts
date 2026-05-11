58c8abc60bbf9238ac61c5718d56c847a9ac94ba
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-05-10 21:01:19 -0500

    feat: add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  providerApiBaseUrl: 'http://localhost:3000/api/v1',
+  providerHomeEndPointPath: '/homes',
+  providerDeviceEndPointPath: '/devices',
+  providerConsumptionsEndPointPath: '/consumptions',
+  logoProviderApiBaseUrl: 'https://img.logo.dev/',
+  logoProviderPublishableKey: 'pk_MaaysUc_QNaCWpZNw7k5UA',
+};
